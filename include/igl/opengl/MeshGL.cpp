31339e957d56080dab2440567ec2703b378153af
Pieter van Klaveren <p.vanklaveren@dotxcontrol.com>
2023-12-06 10:51:36 +0100

    - changed draw_mesh function in meshgl. This is for the new way of performing the simulation of the toolpaths

--- include/igl/opengl/MeshGL.cpp
+++ include/igl/opengl/MeshGL.cpp
@@ -279,22 +279,13 @@ IGL_INLINE void igl::opengl::MeshGL::draw_mesh(bool solid)
     glEnable(GL_POLYGON_OFFSET_FILL);
     glPolygonOffset(1.0, 1.0);
   }
-  // TODO: adjust this to adjust for random paths
-  if (is_simulation == true) { // this was is_print before
-      if (step <= first_printing_step) {
-          glDrawElements(GL_TRIANGLES, 0, GL_UNSIGNED_INT, 0);
-      }
-      else {
-          // if statement to catch when interpolation is needed.
-          glDrawElements(GL_TRIANGLES, 3 * n_element_in_one_F * (step - first_printing_step) - 3 * dev_in_F, GL_UNSIGNED_INT, 0);
-      }
-      
-      //std::cout << "Line 281: # of rows to draw = " << 3 * n_element_in_one_F * (step - 3) << std::endl;
-  }
-  else {
+  if (step == -1) {
       glDrawElements(GL_TRIANGLES, 3 * F_vbo.rows(), GL_UNSIGNED_INT, 0);
-      //std::cout << "Line 290: # of rows to draw = " << 3 * F_vbo.rows() << std::endl;
   }
+  else if (step >= 0) {
+      glDrawElements(GL_TRIANGLES, 3 * number_triangles_per_step * step, GL_UNSIGNED_INT, 0);
+  }
+
   glDisable(GL_POLYGON_OFFSET_FILL);
   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
